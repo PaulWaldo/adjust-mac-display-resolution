774dc6efcb6c46a69c0ce06a512348f6b22773f3
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-20 11:30:45 -0500

    Creates script to build script libraries and push to correct folder

---
+++ build.sh
@@ -0,0 +1,5 @@
+# Compile plain-text AppleScript files to .scpt files and place them in 
+# the user's Script Libraries folder
+osacompile -o ~/Library/Script\ Libraries/My\ Error\ Codes.scpt My\ Error\ Codes.applescript
+osacompile -o ~/Library/Script\ Libraries/My\ Display\ Utilities.scpt My\ Display\ Utilities.applescript
+
