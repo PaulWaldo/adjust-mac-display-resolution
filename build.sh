53f8b7c084ac25b48fecd766ee1b29380ee9a6a4
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-27 15:16:48 -0500

    More robust error handling

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-# Compile plain-text AppleScript files to .scpt files and place them in 
+# Compile plain-text AppleScript files to .scpt files and place them in
 # the user's Script Libraries folder
-osacompile -o ~/Library/Script\ Libraries/My\ Error\ Codes.scpt My\ Error\ Codes.applescript
-osacompile -o ~/Library/Script\ Libraries/My\ Display\ Utilities.scpt My\ Display\ Utilities.applescript
-
+set -e
+USER_SCRIPT_LIBRARY=~/Library/Script\ Libraries
+mkdir -p "$USER_SCRIPT_LIBRARY"
+osacompile -o "$USER_SCRIPT_LIBRARY"/My\ Error\ Codes.scpt My\ Error\ Codes.applescript
+osacompile -o "$USER_SCRIPT_LIBRARY"/My\ Display\ Utilities.scpt My\ Display\ Utilities.applescript
