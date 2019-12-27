a5329bd5377b62d2622b8e49d8d8a746b4335b61
Paul Waldo <paul.w.waldo@gmail.com>
2019-12-27 16:05:26 -0500

    Creates a functioning test environment

--- build_test.sh
+++ build_test.sh
@@ -1,13 +1,12 @@
 set -e
 
 # Set the Script Library path to include the current directory
-export OSA_LIBRARY_PATH=$(pwd)
-#:$OSA_LIBRARY_PATH
+OSA_LIBRARY_PATH=$(pwd):$OSA_LIBRARY_PATH
 echo $OSA_LIBRARY_PATH
 
 # Compile plain-text AppleScript files to .scpt files in the current folder
-my_osacompile -o "My Error Codes.scpt" "My Error Codes.applescript"
-~/bin/my_osacompile -o "My Display Utilities.scpt" "My Display Utilities.applescript"
+./my_osacompile -o "My Error Codes.scpt" "My Error Codes.applescript"
+./my_osacompile -o "My Display Utilities.scpt" "My Display Utilities.applescript"
 
 # Run the main script
-my_osascript "My Display Utilities.scpt"
+./my_osascript "My Display Utilities.scpt"
